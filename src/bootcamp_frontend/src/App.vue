1f0b5785923fe967aa9599eb03ab234f57d5e723
rostyk7 <rostyslav.haidukov@live.com>
2024-06-24 18:35:55 +0200

    changes

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -1,18 +1,3 @@
-<script setup>
-import { ref } from 'vue';
-import { bootcamp_backend } from 'declarations/bootcamp_backend/index';
-let greeting = ref('');
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await bootcamp_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
-}
-</script>
-
 <template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
@@ -23,6 +8,6 @@ async function handleSubmit(e) {
       <input id="name" alt="Name" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <section id="greeting">Hello!</section>
   </main>
 </template>
